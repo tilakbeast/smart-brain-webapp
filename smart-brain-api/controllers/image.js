a67f431e0adc431114b40cf24cb87d65953ee2c0
Tilak Shukla <tilakshukla233@gmail.com>
2024-02-09 21:30:59 +0530

    done

--- smart-brain-api/controllers/image.js
+++ smart-brain-api/controllers/image.js
@@ -6,12 +6,13 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
     app.models.predict({
-        id: "a403429f2ddf4b49b307e318f00e528b",
+        // id: "a403429f2ddf4b49b307e318f00e528b",
+        id: "6dc7e46bc9124c5c8824be4822abe105",
         version: "34ce21a40cc24b6b96ffee54aabff139",
       }, req.body.input)
 
     .then(data => res.json(data))
-    .catch(err => res.status(400).json("couldn't fetch"))
+    .catch(err => res.status(400).json(err))
 }
 
 
